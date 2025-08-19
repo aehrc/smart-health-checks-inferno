3e884f96092a928778b0f34ce6597207de972606
Pavel Rozhkov <prozskov@gmail.com>
2025-08-19 16:26:46 +0200

    Update reference resolution tests and configuration to include additional AU Core profiles

--- lib/smart_health_checks_test_kit/generated/v0.3.0-draft/allergy_intolerance/allergy_intolerance_create_test.rb
+++ lib/smart_health_checks_test_kit/generated/v0.3.0-draft/allergy_intolerance/allergy_intolerance_create_test.rb
@@ -28,46 +28,11 @@ module SmartHealthChecksTestKit
       }
     ]
   },
-  "verificationStatus": {
-    "coding": [
-      {
-        "system": "http://terminology.hl7.org/CodeSystem/allergyintolerance-verification",
-        "code": "unconfirmed",
-        "display": "Unconfirmed"
-      }
-    ]
+  "patient": {
+    "reference": "http://example.com"
   },
-  "type": "allergy",
-  "category": [
-    "environment"
-  ],
-  "code": {
-    "coding": [
-      {
-        "system": "http://snomed.info/sct",
-        "code": "716186003",
-        "display": "No known allergy"
-      }
-    ]
-  },
-  "onsetDateTime": "2023-02-12",
-  "note": [
-    {
-      "text": "noneknown note"
-    }
-  ],
   "reaction": [
     {
-      "substance": {
-        "coding": [
-          {
-            "system": "http://snomed.info/sct",
-            "code": "102263004",
-            "display": "Eggs (edible)"
-          }
-        ],
-        "text": "Egg"
-      },
       "manifestation": [
         {
           "coding": [
