f5328ba7dc4c2dc25f266d7c1a48c4bffce02296
Pavel Rozhkov <prozskov@gmail.com>
2025-11-26 17:08:04 +0100

    Delete all suites

--- lib/smart_health_checks_test_kit.rb
+++ lib/smart_health_checks_test_kit.rb
@@ -1,4 +1,1 @@
 # frozen_string_literal: true
-
-require_relative 'smart_health_checks_test_kit/generated/v0.3.0-draft/smart_health_checks_test_suite'
-require_relative 'smart_health_checks_test_kit/generated/v0.4.0-ci-build/smart_health_checks_test_suite'
