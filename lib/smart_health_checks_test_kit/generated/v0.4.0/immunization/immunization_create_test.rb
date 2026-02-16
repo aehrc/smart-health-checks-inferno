e2d6184fa3cf2466f74319fb3daa6ec248034603
Pavel Rozhkov <prozskov@gmail.com>
2026-02-16 19:01:53 +0100

    Generate the new suite for the 0.4.0 IG (4cb3b6)

---
+++ lib/smart_health_checks_test_kit/generated/v0.4.0/immunization/immunization_create_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'inferno_suite_generator/test_modules/create_test'
+require 'inferno_suite_generator/utils/references_keeper'
+
+module SmartHealthChecksTestKit
+  module SmartHealthChecksV040
+    class ImmunizationCreateTest < Inferno::Test
+      include InfernoSuiteGenerator::CreateTest
+
+      title '(SHALL) Server returns correct Immunization resource from Immunization create interaction'
+      description 'A server SHALL support the Immunization create interaction.'
+
+      input :references_mapping_input,
+            title: 'References Mapping',
+            description: 'Mapping of references to the create resource. Format: {"Patient": ["patient_id1", "patient_id2"], "Condition": ["condition_id1", "condition_id2"]}',
+            type: 'textarea',
+            optional: true
+
+      id :smart_health_checks_v040_immunization_create_test
+
+      def self.demodata
+        @demodata ||= InfernoSuiteGenerator::Generator::IGDemodata.new(
+          YAML.load_file(File.join(File.dirname(__dir__), 'demodata.yml'), aliases: true)
+        )
+      end
+
+      def self.metadata
+        @metadata ||= InfernoSuiteGenerator::Generator::GroupMetadata.new(
+          YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true)
+        )
+      end
+
+      def resource_type
+        'Immunization'
+      end
+
+      def references_keeper
+        @rerefences_keeper = InfernoSuiteGenerator::ReferencesKeeper.get_or_create_instance(url)
+      end
+
+      run do
+        perform_create_test
+      end
+    end
+  end
+end
---
+++ lib/smart_health_checks_test_kit/generated/v0.4.0/medication_statement/medication_statement_create_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'inferno_suite_generator/test_modules/create_test'
+require 'inferno_suite_generator/utils/references_keeper'
+
+module SmartHealthChecksTestKit
+  module SmartHealthChecksV040
+    class MedicationStatementCreateTest < Inferno::Test
+      include InfernoSuiteGenerator::CreateTest
+
+      title '(SHALL) Server returns correct MedicationStatement resource from MedicationStatement create interaction'
+      description 'A server SHALL support the MedicationStatement create interaction.'
+
+      input :references_mapping_input,
+            title: 'References Mapping',
+            description: 'Mapping of references to the create resource. Format: {"Patient": ["patient_id1", "patient_id2"], "Condition": ["condition_id1", "condition_id2"]}',
+            type: 'textarea',
+            optional: true
+
+      id :smart_health_checks_v040_medication_statement_create_test
+
+      def self.demodata
+        @demodata ||= InfernoSuiteGenerator::Generator::IGDemodata.new(
+          YAML.load_file(File.join(File.dirname(__dir__), 'demodata.yml'), aliases: true)
+        )
+      end
+
+      def self.metadata
+        @metadata ||= InfernoSuiteGenerator::Generator::GroupMetadata.new(
+          YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true)
+        )
+      end
+
+      def resource_type
+        'MedicationStatement'
+      end
+
+      def references_keeper
+        @rerefences_keeper = InfernoSuiteGenerator::ReferencesKeeper.get_or_create_instance(url)
+      end
+
+      run do
+        perform_create_test
+      end
+    end
+  end
+end
